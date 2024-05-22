6b3967b6fa931fb1c33d96384ba6f74c46c5eaca
Alexandr Kobrin <alexk@canva.com>
2024-05-23 05:08:28 +1000

    update arch

--- Formula/wiper.rb
+++ Formula/wiper.rb
@@ -4,8 +4,17 @@
 class Wiper < Formula
   desc "Cross-platform space inspector"
   homepage "https://github.com/ikebastuz/wiper"
-  url "https://github.com/ikebastuz/wiper/releases/download/v0.2.0/wiper.tar.gz"
-  sha256 "7b870a52e152ef71efd6ba886a9479a09243510f095413fe0eadd8d40907f6d4"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/ikebastuz/wiper/releases/download/v0.2.0/wiper-x86_64-apple-darwin.tar.gz"
+      sha256 "8b427d6f994f1f87eb928eb72c2eef8d4ce4718b168d6f9eb6bbda6dbb7c224f"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/ikebastuz/wiper/releases/download/v0.2.0/wiper-aarch64-apple-darwin.tar.gz"
+      sha256 "dd16c68f81d033183a46ad6907349ad7af0f0a56f194327d97a0da45136010d9"
+    end
+  end
+
   version "0.2.0"
 
   def install
