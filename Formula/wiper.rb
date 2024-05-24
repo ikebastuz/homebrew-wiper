3a59637d1392456656e774ae72f0396c9f38de82
Alexandr Kobrin <alexk@canva.com>
2024-05-25 05:42:39 +1000

    link fix

--- Formula/wiper.rb
+++ Formula/wiper.rb
@@ -7,10 +7,10 @@ class Wiper < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/ikebastuz/wiper/releases/download/v0.2.1/wiper-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/ikebastuz/wiper/releases/download/v0.2.1/wiper-x86_64-apple-darwin.tar.gz"
       sha256 "469ddc8fb9b71f1a860ddf55dd5cc121b30caa094a7e82d9b830944dab561502"
     elsif Hardware::CPU.arm?
-      url "https://github.com/ikebastuz/wiper/releases/download/v0.2.1/wiper-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/ikebastuz/wiper/releases/download/v0.2.1/wiper-aarch64-apple-darwin.tar.gz"
       sha256 "bba5ebb52086c5271405a35ac792e86521a0dfea22b453e0320f8c94ce591569"
     end
   end
